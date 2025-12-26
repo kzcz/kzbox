4d2724f426a20181812e8099cf901279b99f9be2
Cursed <cursitical@gmail.com>
2025-12-25 21:18:04 -0300

    made build.zig even smaller

--- build.zig
+++ build.zig
@@ -4,16 +4,18 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const strip = b.option(bool, "strip", "Whether to strip the binary") orelse false;
-    var as_buf = try std.array_list.Aligned(u8, null).initCapacity(b.allocator, 16 * 1024);
-    try as_buf.print(b.allocator, "{s}", .{"pub const Applet = struct { name: []const u8, help: []const u8, main: *const fn (args: [][:0]u8) anyerror!u8 };\npub fn load_applet(comptime name: []const u8, root: type) Applet {\n    return .{ .name = name, .help = root.help, .main = root.main };\n}\npub var _ap = [_]Applet{\n"});
+    var as_buf = try std.Io.Writer.Allocating.initCapacity(b.allocator, 16 * 1024);
+    defer as_buf.deinit();
+    var asw = as_buf.writer;
+    try asw.writeAll("pub const Applet = struct { name: []const u8, help: []const u8, main: *const fn (args: [][:0]u8) anyerror!u8 };\npub fn load_applet(comptime name: []const u8, root: type) Applet {\n    return .{ .name = name, .help = root.help, .main = root.main };\n}\npub var _ap = [_]Applet{\n");
     var dir = (try b.path("").getPath3(b, null).openDir("applets", .{ .iterate = true })).iterate();
 
     while (try dir.next()) |entry| {
         if (entry.name.len < 4 or !std.mem.eql(u8, entry.name[entry.name.len - 4 ..], ".zig")) continue;
-        try as_buf.print(b.allocator, "\tload_applet(\"{s}\", @import(\"applets/{s}\")),\n", .{ entry.name[0 .. entry.name.len - 4], entry.name });
+        try asw.print("\tload_applet(\"{s}\", @import(\"applets/{s}\")),\n", .{ entry.name[0 .. entry.name.len - 4], entry.name });
     }
-    try as_buf.print(b.allocator, "\n}};\n", .{});
-    const write_applets = b.addWriteFile(b.pathFromRoot("applets.zig"), try as_buf.toOwnedSlice(b.allocator));
+    try asw.writeAll("\n}};\n");
+    const write_applets = b.addWriteFile(b.pathFromRoot("applets.zig"), try as_buf.toOwnedSlice());
     const root = b.createModule(.{ .root_source_file = b.path("kzbox.zig"), .target = target, .optimize = optimize, .strip = strip });
     root.addImport("kzlib", b.createModule(.{ .root_source_file = b.path("lib.zig"), .target = target, .optimize = optimize, .strip = strip }));
     const exe = b.addExecutable(.{ .name = "kzbox", .root_module = root, .version = std.SemanticVersion.parse(@import("build.zig.zon").version) catch unreachable });
